2e4f407fa6e0f463760731eb0fcdc6a36bbdc1c0
Ujjair-khilji <ujjairkhilji@gmail.com>
2023-03-15 03:04:57 +0530

    add js file

---
+++ reveise.js
@@ -0,0 +1,216 @@
+// // // // // // // // // var a= "xyz"
+// // // // // // // // // a="htt"//reassign
+// // // // // // // // // console.log(a)
+// // // // // // // // // var a= "shsh"//varible name bi same likh sakha
+// // // // // // // // // console.log(a)
+// // // // // // // // // let v= "name"
+// // // // // // // // // console.log(v)
+// // // // // // // // // v="sry"
+// // // // // // // // // console.log(v)//let ham name same nhi lekh skte he
+// // // // // // // // // // let v= ""
+// // // // // // // // // const s = "fs"
+// // // // // // // // // console.log(s) 
+// // // // // // // // // // const s = "fs"
+// // // // // // // // // // console.log(s)isme reassign nhi kr skte or name same nhi likh skte
+// // // // // // // // // //function
+// // // // // // // // // function d(){
+// // // // // // // // //     console.log("fayyaz is a good boy")
+// // // // // // // // // }
+// // // // // // // // // d()
+// // // // // // // // // // data types oppreter
+// // // // // // // // // var x = 'yes'
+// // // // // // // // // console.log(x)
+// // // // // // // // // console.log(typeof x)
+// // // // // // // // // var b = 8
+// // // // // // // // // console.log(typeof b)
+// // // // // // // // // var z = "uuu"
+// // // // // // // // // console.log(z)
+// // // // // // // // // var x = "html"
+// // // // // // // // // console.log(    x)
+// // // // // // // // // var  name = "uzair";
+// // // // // // // // // var age = 16; 
+// // // // // // // // // console.log(name, age);
+// // // // // // // // const name = "uzair";
+// // // // // // // // const age = 16;
+// // // // // // // // const className = "9th";
+// // // // // // // // const school = "m.k.a.m";
+// // // // // // // // const introduction = `hello my name is ${name} and my age is ${age}.i am in class ${className} from school ${school}`;
+// // // // // // // // // console.log(name,age,className,school);
+// // // // // // // // // introduction = 1;
+// // // // // // // // console.log(introduction);
+// // // // // // // // const name1 = "uzair";
+// // // // // // // // const age1 = 16;
+// // // // // // // // const madarsaName = "baradari madarsa";
+// // // // // // // // const subject = "quran";
+// // // // // // // // const introduction1 = `hello my name is ${name1} and my age is ${age1}. i am in madarsa ${madarsaName} and subject name is ${subject}`;
+// // // // // // // // console.log(introduction1);
+// // // // // // // // //family introduction
+// // // // // // // // const name2 = "uzair";
+// // // // // // // // const fName = "anwar";
+// // // // // // // // const brother = "zaid";
+// // // // // // // // const introduction2 = `hello my name is ${name2} and my father name is ${fName} and my brother name is ${brother}`;
+// // // // // // // // console.log(introduction2)
+// // // // // // // // //freinds introduction
+// // // // // // // // const name3 = "uzair";
+// // // // // // // // const freind1 = "fayyaz";
+// // // // // // // // const friend2 = "danish";
+// // // // // // // // const friend3 = "moffu";
+// // // // // // // // const introduction3 = `hello my name is ${name3} and i have three friends 1st ${freind1} 2nd ${friend2} 3rd ${friend3}`;
+// // // // // // // // console.log(introduction3);
+// // // // // // // // arithmetic oppreter
+// // // // // // // var a = 10;
+// // // // // // // var b = 14;
+// // // // // // // var c = (a + b);
+// // // // // // // console.log(c);
+// // // // // // // var a = 19;
+// // // // // // // var b = 15;
+// // // // // // // var c = (a - b);
+// // // // // // // console.log(c);
+// // // // // // // var a = 5;
+// // // // // // // var b = 3; 
+// // // // // // // var c = (a * b);
+// // // // // // // console.log(c);
+// // // // // // // var a = 61;
+// // // // // // // var b = 2;
+// // // // // // // var c = (a % b);
+// // // // // // // console.log(c)
+// // // // // // // var a = 5;
+// // // // // // // var b = 3 ;
+// // // // // // // var c = (a ** b);
+// // // // // // // console.log(c);
+// // // // // // // var a = 5;
+// // // // // // // var b = 3;
+// // // // // // // var c = (a / b);
+// // // // // // // console.log(c);
+// // // // // // // COMPARISON OPARATOR
+// // // // // // var a = 1 ;
+// // // // // // var b = 2;
+// // // // // // var c = (a < b);
+// // // // // // console.log(c);
+// // // // // // var a = 12;
+// // // // // // var b = 13;
+// // // // // // var c = (a > b);
+// // // // // // console.log(c);
+// // // // // // var a = 4;
+// // // // // // var b = 5;
+// // // // // // var c = (a == b);
+// // // // // // console.log(c);
+// // // // // // var a = 4;
+// // // // // // var b = 4;
+// // // // // // var c = (a == b);
+// // // // // // console.log(c);
+// // // // // // var a = 3;
+// // // // // // var b = 34;
+// // // // // // var c = (a === b);
+// // // // // // console.log(c);
+// // // // // // var a = 5;
+// // // // // // var b = 5;
+// // // // // // var c = (a === b);
+// // // // // // console.log(c);
+// // // // // // var a = 3;
+// // // // // // var b = 6;
+// // // // // // var c = (a != b);
+// // // // // // console.log(c);
+// // // // // // var a = 90;
+// // // // // // var b = 90;
+// // // // // // var c = (a != b);
+// // // // // // console.log(c);
+// // // // // // var a = 50;
+// // // // // // var b = "60";
+// // // // // // var c = (a !== b);
+// // // // // // console.log(c);
+// // // // // var a = 1;
+// // // // // var b = 5;
+// // // // // if(a < b){
+// // // // //     console.log("hyy my name is uzair")
+// // // // // } 
+// // // // // var a = 5;
+// // // // // var b = 7;
+// // // // // if(a != b){
+// // // // //     console.log("hello how are u" )
+// // // // // }
+
+// // // //logical oparator
+// // // // // var a = 10;
+// // // // // var b = 16;
+// // // // // var c = (!a );
+// // // // // console.log(c)
+// // // // var per = 40;
+
+//  if(per >= 80 && per <= 100){
+//      console.log("you are 1st division")
+//  }else if (per >= 60 && per <= 70){
+//    console.log("you are 2nd division")
+//  }else if (per >= 40 && per <= 50){
+//      console.log("you are 3rd division")
+//  }
+//  else if (per >= 35 && per <= 40){
+//      console.log("you are pass")
+//  }
+//  else if (per >= 10 &&per <= 30){   
+//       console.log("you are fail")
+//      };
+
+// // // var a = 100;
+// // // var b;
+// // // (a == 100)?  b = "true" : b = "false";
+// // // console.log(b);
+// // // var a = 100;
+// // // var b;
+// // // (a < 50)? b = "true" : b = "false";
+// // // console.log(b);
+// // // var a = 100;
+// // // var b;
+// // // b = "value is " + (a > 110? "true" : "false");
+// // // console.log(b);
+// // // var a = 100;
+// // // var b;
+// // // b = "value is " + (a > 20? "true" : "false");
+// // // console.log(b)
+// // // const day = 1;
+
+// // // switch(day) {
+// // //     case 0:
+// // //         console.log("today is monday");
+// // //      break   
+// // //      case 1:
+// // //         console.log("today is tuesday");
+// // //      break   
+    
+// // //      case 2:
+// // //         console.log("today is wenesday");
+// // //      break   
+    
+// // //      case 3:
+// // //         console.log("today is thursday");
+// // //      break        
+    
+// // //      case 4:
+// // //         console.log("today is friday");
+// // //      break   
+    
+// // //      case 5:
+// // //         console.log("today is saturday");
+// // //      break
+// // //         default:
+// // //             console.log("enter the valid day")
+    
+// // // };
+// // // //alert ("hyy my name is uzair");
+// // // var a = 40;
+// // // var b = 20;
+// // // alert (b);
+// // //const con = confirm("hello how are you ");
+// // var a = prompt ("what is your name");
+ 
+// //  console.log (a);
+// var a = "uzair";
+// var a =  4
+// console.log(typeof (a));
+function book(fname : lname){
+    console.log("hello everyone")
+};
+
+
+book();
+console.log ("my name is UZAIR");
